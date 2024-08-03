e0e73ff3e4a8dc1509de63b2cd1a35ccaa1af542
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-08-03 15:13:05 +0100

    install scroll-lock, create logo and navigation items and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import Header from "./components/Header"
+
 const App = () => {
   return (
     <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
     </>
   )
 }
